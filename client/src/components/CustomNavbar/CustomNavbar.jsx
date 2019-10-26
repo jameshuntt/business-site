81376e31e85f157b201a7a3790569c2e79d06d6d
jameshuntt <jh222807@live.sunysccc.edu>
2019-10-25 21:06:05 -0400

    added sidenav files

--- client/src/components/CustomNavbar/CustomNavbar.jsx
+++ client/src/components/CustomNavbar/CustomNavbar.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Backdrop from '../SideNav/Backdrop/Backdrop';
+import SideDrawer from '../SideNav/SideDrawer/SideDrawer';
+import drawerToggleButton from '../SideNav/SideDrawer/DrawerToggleButton';
 import './CustomNavbar.scss';
 
 export default class JamesNavbar extends Component {
-    render() {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+          sideDrawerOpen: false
+      }
+  }
+
+  drawerToggleClickHandler = () => {
+      this.setState((prevState) => {
+          return {sideDrawerOpen: !prevState.sideDrawerOpen};
+      });
+  };
+
+  backdropClickHandler = () => {
+      this.setState({sideDrawerOpen: false});
+  };
+
+  render() {
+      let backdrop;
+      if (this.state.sideDrawerOpen) {
+          backdrop = <Backdrop click={this.backdropClickHandler} />
+      }
         return (
             <div className="navbar-container">
               <div className="navbar-buttons">
@@ -21,6 +46,11 @@ export default class JamesNavbar extends Component {
                 <Link to="/CurrentlyServicing"><Button variant="outline-light">Servicing</Button></Link>
                 <Link to="/AllServices"><Button variant="outline-light">Products</Button></Link>
               </div>
+                <drawerToggleButton className="burger-menu-icon" onClick={this.drawerToggleClickHandler} />
+                {/*<input className="burger-menu-icon" type="image" src="/assets/hamburger-menu-icon.jpg" alt="burger" onClick={this.drawerToggleClickHandler} />*/}
+                <div className="spacer2" />
+                <SideDrawer show={this.state.sideDrawerOpen} />
+                {backdrop}
             </div>
         )
     }
