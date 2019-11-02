d6b86106bf9c96130fe40a2beed1a88e64831906
Chase Heavner <charlesrheavner@gmail.com>
2019-11-02 17:16:38 -0400

    created the navigation drop down still have some bugs to work out

--- client/src/components/CustomNavbar/CustomNavbar.jsx
+++ client/src/components/CustomNavbar/CustomNavbar.jsx
@@ -4,23 +4,30 @@ import { Link } from 'react-router-dom';
 import './CustomNavbar.scss';
 
 export default class JamesNavbar extends Component {
-    render() {
+   
+  state = {
+    isHamburgerOpen: false,
+  }
+   
+  render() {
+    console.log(this.state.isHamburgerOpen)
+    const isOpen = this.state.isHamburgerOpen;
         return (
             <div className="navbar-container">
               <div className="navbar-brand">
                 <img src="/assets/Logo.png" />
                 <img src="/assets/Logoo.png" />
               </div>
-              <div className="navbar-links">
-                <a><Link to="/">Home</Link></a>
+              <div className={ isOpen ? "navbar-links" : "navbar-links-open" }>
+                <a onClick={ () => this.setState({isOpen: false})}><Link to="/" >Home</Link></a>
                 <a><Link to="/About">About</Link></a>
                 <a><Link to="/CurrentlyServicing">Servicing</Link></a>
                 <a><Link to="/AllServices">Products</Link></a>
               </div>
-              <div className="Hamburger">
-                <div className="Line"></div>
-                <div className="Line"></div>
-                <div className="Line"></div>
+              <div className={ isOpen ? "hamburger" : "hamburger-open"} onClick={ () => this.setState({isHamburgerOpen: !isOpen})}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
               </div>
             </div>
         )
