44c62a66e960b2cc78d0d6c7ac9e26128a6302a9
jameshuntt <jh222807@live.sunysccc.edu>
2019-11-22 17:45:36 -0500

    last commit before deleting and cloning to get the development branch

--- client/src/components/Contact/Contact2.jsx
+++ client/src/components/Contact/Contact2.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Contact2.scss';
+//import './Contact2.scss';
 
 export default class Contact2 extends Component {
     constructor(props) {
@@ -11,6 +11,8 @@ export default class Contact2 extends Component {
             lname: '',
             email: '',
             message: '',
+            mailSent: false,
+            error: null
          }
         
         }
@@ -21,30 +23,35 @@ export default class Contact2 extends Component {
           }
     render() {
         return (
-            <div>
+            <div style={{ paddingTop: '20vh'}}>
                 <form action="#">
-                <label>First Name</label>
-                <input type="text"id="fname" name="firstname" placeholder="Your name.."
-                value={this.state.fname}
-                onChange={e=>this.setState({ fname:e.target.value})}
-                />
-                <label>Last Name</label>
-                <input type=" text"id="lname" name="lastname" placeholder="Your last name.."
-                value={this.state.lname}
-                onChange={e=>this.setState({ lname:e.target.value})}
-                />
-                <label>Email</label>
-                <input type="email" id="email" name="email" placeholder="Your email"
-                value={this.state.email}
-                onChange={e=>this.setState({ email:e.target.value})}
-                />
-                <label>Message</label>
-                <textarea id="message" name="message" placeholder="Write something.."
-                onChange={e=>this.setState({ message:e.target.value})}
-                value={this.state.message}
-                ></textarea>
-                <input type="submit" onClick={e=>this.handleFormSubmit(e)} value="Submit"/>
-                </form>
+                    <label>First Name</label>
+                    <input type="text"id="fname" name="firstname" placeholder="Your name.."
+                    value={this.state.fname}
+                    onChange={e=>this.setState({ fname:e.target.value})}
+                    />
+                    <label>Last Name</label>
+                    <input type=" text"id="lname" name="lastname" placeholder="Your last name.."
+                    value={this.state.lname}
+                    onChange={e=>this.setState({ lname:e.target.value})}
+                    />
+                    <label>Email</label>
+                    <input type="email" id="email" name="email" placeholder="Your email"
+                    value={this.state.email}
+                    onChange={e=>this.setState({ email:e.target.value})}
+                    />
+                    <label>Message</label>
+                    <textarea id="message" name="message" placeholder="Write something.."
+                    onChange={e=>this.setState({ message:e.target.value})}
+                    value={this.state.message}
+                    ></textarea>
+                    <input type="submit" onClick={e=>this.handleFormSubmit(e)} value="Submit"/>
+                    <div>
+                    {this.state.mailSent&&
+                    <div>Thank you for contcting us.</div>
+                    }
+                    </div>
+                    </form>
                 </div>
         )
     }
